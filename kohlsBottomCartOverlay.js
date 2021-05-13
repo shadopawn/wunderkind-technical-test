23319ec7780f4644ff4524e16ed4ed858fdea0fe
Daniel Litt <daniel_litt@comcast.net>
2021-05-13 15:10:36 -0400

    Finish initial version of modal

--- kohlsBottomCartOverlay.js
+++ kohlsBottomCartOverlay.js
@@ -1,25 +1,49 @@
-let dropDownCart = document.querySelector(".tr_phase2_shopping-bag_content");
+let dropDownCart = document.querySelector(".tr_phase2_sub_header");
 console.log(dropDownCart);
 
+let cartClone = dropDownCart.cloneNode(true);
+
 let container = document.querySelector("#container");
-//contentDiv.innerHTML += miniCart.innerHTML;
-
-console.log(container.offsetHeight);
-
 window.onscroll = () => {
     let bottomOffset = 1000;
     let pastBottomTrigger = (window.innerHeight + window.pageYOffset) >= container.offsetHeight - bottomOffset;
     if (pastBottomTrigger){
-        alert(1);
+        showModal();
     }
 };
 
-let modalBackground = document.createElement('div');
-modalBackground.className = "bottom-cart-modal";
-document.body.appendChild(modalBackground);
+let modalContainer = document.createElement('div');
+modalContainer.className = "bottom-cart-modal";
+document.body.appendChild(modalContainer);
+
+modalContainer.appendChild(cartClone);
+
+let closeButton = document.createElement('span');
+closeButton.className = "close";
+closeButton.innerHTML = "&times;";
+cartClone.prepend(closeButton);
+
+closeButton.onclick = () => {
+    hideModal();
+}
 
 let cartStyleLink  = document.createElement('link');
 cartStyleLink.rel  = 'stylesheet';
 cartStyleLink.type = 'text/css';
 cartStyleLink.href = 'http://127.0.0.1:5500/cartStyle.css';
 document.head.appendChild(cartStyleLink);
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalContainer) {
+        hideModal();
+    }
+}
+
+function hideModal(){
+    modalContainer.style.display = "none";
+}
+
+function showModal(){
+    modalContainer.style.display = "flex";
+}
